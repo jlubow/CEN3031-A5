ca3fa76abf2c9f0c30502b05295a64afc0ca1ec9
jlubow <joshlubow@gmail.com>
2017-09-21 21:28:14 -0400

    Update listings.client.factory.js

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -6,19 +6,19 @@ angular.module('listings').factory('Listings', ['$http',
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:8080/api/listings', listing);
+        return $http.post('https://' + window.location.hostname + '/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
+        return $http.get('https://' + window.location.hostname + '/api/listings' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
+        return $http.put('https://' + window.location.hostname + '/api/listings' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $http.delete('https://' + window.location.hostname + '/api/listings' + id);
       }
     };
 
